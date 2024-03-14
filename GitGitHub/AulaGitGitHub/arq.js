4c610bafbdc35631a740851e26e3076d3dc2c0e9
Lucas Rodrigues <rodrigueslucas062@gmail.com>
2024-03-14 09:38:24 -0300

    ✨ Function soma

---
+++ GitGitHub/AulaGitGitHub/arq.js
@@ -0,0 +1,10 @@
+function Soma () {
+    var n1, n2
+    n1 = document.getElementById ('Primeiro Numero').value
+    n2 = document.getElementById ('Segundo Numero').value
+
+    n1 = parseInt (n1)
+    n2 = parseInt (n2)
+
+    Soma = n1 + n2 
+}
